122708c6dc71659a29363f2ce2aec8cc2986a29a
Johan Wigmo <johan@wigmo.dev>
2026-09-25 09:33:11 +0200

    Fix treesitter issue

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 -- lua/plugins/telescope.lua
 return {
-   'nvim-telescope/telescope.nvim', tag = '0.1.8', 
+   'nvim-telescope/telescope.nvim', branch = 'master',
    
     config = function()
         require('telescope').setup({})
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,56 @@
+-- nvim-treesitter (main branch, Neovim 0.12+)
+-- Installs parsers/queries only; highlighting/indent are enabled below.
+local languages = {
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "markdown",
+    "markdown_inline",
+    "javascript",
+    "html",
+    "kotlin",
+    "swift",
+    "todotxt",
+}
+
 return {
-    'nvim-treesitter/nvim-treesitter', 
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
 
-    config = function() 
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { 
-                "lua", 
-                "vim", 
-                "vimdoc", 
-                "query", 
-                "javascript", 
-                "html", 
-                "kotlin", 
-                "swift",
-                "todotxt"
-            },
-            sync_install = false,
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },  
+    config = function()
+        -- Custom parser: todotxt
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "TSUpdate",
+            callback = function()
+                require("nvim-treesitter.parsers").todotxt = {
+                    install_info = {
+                        url = "https://github.com/arnarg/tree-sitter-todotxt",
+                        branch = "main",
+                    },
+                }
+            end,
         })
 
-        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        parser_config.todotxt = {
-            install_info = {
-                url = "https://github.com/arnarg/tree-sitter-todotxt",
-                files = { "src/parser.c" },
-                branch = "main"
-            },
-            filetype = "todotxt"
-        }
+        require("nvim-treesitter").install(languages)
+
+        -- Enable highlighting + indent wherever a parser exists
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+                if not pcall(vim.treesitter.start, args.buf) then return end
+                vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
 
         local todo_root = vim.fn.expand("$TODO_ROOT")
         vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
             pattern = {
                 todo_root .. "/*.txt",
-                todo_root .. "/**/*.txt"
+                todo_root .. "/**/*.txt",
             },
-            command = "set filetype=todotxt"
+            command = "set filetype=todotxt",
         })
-    end
+    end,
 }
-
